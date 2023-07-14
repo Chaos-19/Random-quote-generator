39f6d7412c45c3bd444d7efe04d1300da2a0fe4b
kalgetachew375@gmail.com <kalgetachew375@gmail.com>
2023-07-14 04:51:37 +0300

    saving quote on repository

--- main.js
+++ main.js
@@ -3,40 +3,33 @@ const author = document.querySelector('#author');
 
 const btnGenerate = document.querySelector('#generate');
 
-var randomQuotes = [];
+//var randomQuotes = [];
 let index = 0;
-const xhttp = new XMLHttpRequest();
 
+//const xhttp = new XMLHttpRequest();
+
+/*
 xhttp.onreadystatechange = function() {
 
   if (xhttp.readyState == 4 && xhttp.status == 200) {
     let response = xhttp.responseText;
     randomQuotes = JSON.parse(response);
-
+    console.log(response);
   }
 }
 xhttp.open('Get', "https://type.fit/api/quotes", true);
-xhttp.send();
+xhttp.send();*/
 
 btnGenerate.addEventListener('click', () => {
 
   if (randomQuotes.length > 1) {
     quoteBorde.textContent = randomQuotes[index].text;
     if (randomQuotes[index].author) {
-  author.textContent = randomQuotes[index].author;
-    }else{
+      author.textContent = randomQuotes[index].author;
+    } else {
       author.textContent = "unknown";
     }
     ++index;
-  } else {
-    quoteBorde.textContent = "Loading the quote content......";
-    author.textContent = 'Loading...';
-    ++index;
   }
+})
 
-
-});
-/*
-function getQuote() {
-  return randomQuotes.length > index ? [randomQuotes[index].text, randomQuotes.author] : ["Error"];
-}*/
